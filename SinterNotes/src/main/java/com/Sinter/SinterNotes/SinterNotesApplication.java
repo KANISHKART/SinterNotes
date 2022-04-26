090c5bb5e4878a3f3b49d8dab3629a1890efa02c
KANISHKART <kanishkrish123@gmail.com>
2022-04-26 23:32:24 +0530

    First one

---
+++ SinterNotes/src/main/java/com/Sinter/SinterNotes/SinterNotesApplication.java
@@ -0,0 +1,13 @@
+package com.Sinter.SinterNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinterNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinterNotesApplication.class, args);
+	}
+
+}
---
+++ SinterNotes/src/test/java/com/Sinter/SinterNotes/SinterNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sinter.SinterNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinterNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
