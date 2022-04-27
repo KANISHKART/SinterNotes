de1c7b712ed01b86b9c3c938784ee71582ca1e9c
KANISHKART <kanishkrish123@gmail.com>
2022-04-27 14:55:11 +0530

    Register

--- SinterNotes/src/app/components/register/register.component.ts
+++ SinterNotes/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  registerForm!: FormGroup
+
+  constructor(private formBuild:FormBuilder) {
+    this.registerForm= this.formBuild.group({
+      username: new FormControl('',Validators.required),
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',Validators.required),
+      rpassword: new FormControl('',Validators.required)
+    },
+    {
+      Validators: this.confirmedValidator('password','rpassword')
+    }
+    );
+   }
 
   ngOnInit(): void {
   }
 
+  register(){
+      alert("in login!!")
+  }
+
+  onReset(){
+    this.registerForm.reset();
+  }
+
+  confirmedValidator(controlName: string, matchingControlName :string){
+      return (formGroup:FormGroup)=>{
+        const control= formGroup.controls[controlName];
+        const matchingControl= formGroup.controls[matchingControlName];
+        if(matchingControl.errors && !matchingControl.errors['confirmedValidator']){
+          return;
+        }
+        if(control.value !==matchingControl.value){
+            matchingControl.setErrors({ confirmedValidator: true})
+        }else{
+          matchingControl.setErrors(null);
+        }
+
+      }
+  }
 }
