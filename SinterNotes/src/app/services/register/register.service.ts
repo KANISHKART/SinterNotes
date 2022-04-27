3335c1711677e726134330700397deb98a074e6b
KANISHKART <kanishkrish123@gmail.com>
2022-04-27 15:29:07 +0530

    UI , UX changes

--- SinterNotes/src/app/services/register/register.service.ts
+++ SinterNotes/src/app/services/register/register.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  adduser(data :any){
+    let reqHeaders=new HttpHeaders({
+      'Content-type':'application/json'
+    });
+    const req=new HttpRequest('POST', environment.userController+'/persist',data,{
+      headers: reqHeaders,
+      reportProgress:true
+    });
+
+
+  }
 }
